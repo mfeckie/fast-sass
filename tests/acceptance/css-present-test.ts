28b0d89f06744d9c74638c43c068957c0f9a7542
Martin Feckie <mfeckie@gmail.com>
2020-12-10 07:57:26 +0800

    Add tests for CSS presence

---
+++ tests/acceptance/css-present-test.ts
@@ -0,0 +1,50 @@
+import { module, test } from "qunit";
+import { visit, currentURL } from "@ember/test-helpers";
+import { setupApplicationTest } from "ember-qunit";
+
+function getStyleSheet(name: string): CSSStyleSheet | undefined {
+  const stylesheets: CSSStyleSheet[] = [];
+  for (let i = 0; i < document.styleSheets.length; i++) {
+    stylesheets.push(document.styleSheets[i]);
+  }
+
+  return stylesheets.find((stylesheet) =>
+    stylesheet.href.match(new RegExp(name))
+  );
+}
+
+function hasRule(stylesheet: CSSStyleSheet | undefined, name: string) {
+  if (!stylesheet) {
+    return false;
+  }
+  const rules: CSSStyleRule[] = [];
+  for (let i = 0; i < stylesheet.cssRules.length; i++) {
+    rules.push(stylesheet.cssRules[i] as CSSStyleRule);
+  }
+  return rules.find((rule) => rule.selectorText.match(new RegExp(name)));
+}
+
+module("Acceptance | css present", function (hooks) {
+  setupApplicationTest(hooks);
+
+  test("Pods CSS present", async function (assert) {
+    await visit("/");
+    const dummyStyles = getStyleSheet("dummy.css");
+
+    assert.ok(hasRule(dummyStyles, ".TestComponent"));
+  });
+
+  test("Global CSS present", async function (assert) {
+    await visit("/");
+    const dummyStyles = getStyleSheet("dummy.css");
+
+    assert.ok(hasRule(dummyStyles, ".GlobalAppCSS"));
+  });
+
+  test("OutputPath CSS present", async function (assert) {
+    await visit("/");
+    const dummyStyles = getStyleSheet("output-path.css");
+
+    assert.ok(hasRule(dummyStyles, ".OutputPathCSS"));
+  });
+});
